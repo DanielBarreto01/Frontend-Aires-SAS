b4484a0914c8f4c91894cdb3e4ed619fad89fbe5
Anderseon200 <anderson.barreto01@uptc.edu.co>
2024-10-13 22:32:10 -0500

    Spinner add login

--- src/components/login/AuthContext.js
+++ src/components/login/AuthContext.js
@@ -5,18 +5,35 @@ export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
     const [authToken, setAuthToken] = useState(null);
+
     const handleLogin = async (userName, password) => {
-    const response = await axios.post(`${process.env.REACT_APP_BCKEND}/login`, {userName, password }, {
-        headers :{
-            'Content-Type': 'application/json',
-            'Accept': '*/*'
+        try {
+            const response = await axios.post(`${process.env.REACT_APP_BCKEND}/login`, { userName, password }, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': '*/*'
+                }
+            });
+            return response.data.token; // Retorna el token si la solicitud es exitosa
+        } catch (error) {
+            // Manejo de errores para que se pueda capturar en Login
+            if (error.response) {
+                if (error.response.status === 403) {
+                    throw new Error('Credenciales incorrectas. Por favor, intenta nuevamente.');
+                } else {
+                    throw new Error(error.response.data.message || 'Error en la solicitud.'); // Mensaje genérico para otros errores
+                }
+            } else if (error.request) {
+                throw new Error('El servidor no respondió. Por favor, verifica tu conexión.');
+            } else {
+                throw new Error('Error al realizar la solicitud: ' + error.message);
+            }
         }
-    });
-    return response.data.token;
     };
+
     return (
         <AuthContext.Provider value={{ authToken, handleLogin }}>
-          {children}
+            {children}
         </AuthContext.Provider>
-      );
+    );
 };
