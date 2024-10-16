bf51dcd3f4ab7471ae04a61cbe39f73b849d9c6f
Anderseon200 <anderson.barreto01@uptc.edu.co>
2024-10-16 00:00:47 -0500

    Revert "styles navbar"
    This reverts commit 624627b2bbc04313ecdb00ebe94cd7e0ecd45fa8.

--- src/components/home/AdminDashboard.js
+++ src/components/home/AdminDashboard.js
@@ -16,12 +16,12 @@ function AdminDashboard() {
     const [selectedComponent, setSelectedComponent] = useState(null);
     const [showLogoutModal, setShowLogoutModal] = useState(false);
     const [loading, setLoading] = useState(false);  // Estado de carga
-    const [, setIsTokenChecked] = useState(false);
+    const [isTokenChecked, setIsTokenChecked] = useState(false);
     const [menuOpen, setMenuOpen] = useState(false);
 
     useEffect(() => {  // Mostrar el spinner
         const token = localStorage.getItem('authToken');
-        if (token !== null) {//&& jwtDecode(token).exp*1000 >  Date.now()
+        if (token !== null ) {//&& jwtDecode(token).exp*1000 >  Date.now()
             const decodedToken = jwtDecode(token);
             setUserData({
                 name: decodedToken.name,
@@ -84,140 +84,140 @@ function AdminDashboard() {
     // if (!isTokenChecked) {   
     //     return null;  
     // }  
-
-
+        
+  
     // if (!isTokenChecked) {   
     //     return null;  
     // }  
 
     return (
         <div className="container-fluid" style={{ height: '100vh' }}>
-            {/* Navbar para pantallas pequeñas */}
-            <Navbar bg="light" expand="lg" className="border-bottom d-lg-none">
-                <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setMenuOpen(!menuOpen)} className =" ms-2">
-                    <FontAwesomeIcon icon={faBars} />
-                </Navbar.Toggle>
-                <Navbar.Brand href="#" className="mx-auto d-flex align-items-center">
-                    <img src={logo} alt="Profile" className="profile-img" />
-                    <span className="brand-text">P&A Aires Acondicionados</span>
-                </Navbar.Brand>
-                <Navbar.Collapse id="basic-navbar-nav" className={`slide-menu ${menuOpen ? 'open' : ''}`}>
-                    <Nav className="me-auto">
-                        <Nav.Link href="#" onClick={() => setSelectedComponent('Home')}>
-                            <FontAwesomeIcon icon={faHome} style={{ marginRight: '10px' }} />
-                            Home
-                        </Nav.Link>
-                        <Nav.Link href="#" onClick={() => setSelectedComponent('Usuarios')}>
-                            <FontAwesomeIcon icon={faUsers} style={{ marginRight: '10px' }} />
-                            Usuarios
-                        </Nav.Link>
-                        <Nav.Link href="#">
-                            <FontAwesomeIcon icon={faCalendarDays} style={{ marginRight: '10px' }} />
-                            Equipos
-                        </Nav.Link>
-                        <Nav.Link href="#">
-                            <FontAwesomeIcon icon={faCalendarDays} style={{ marginRight: '10px' }} />
-                            Clientes
-                        </Nav.Link>
-                        <Nav.Link href="#">
-                            <FontAwesomeIcon icon={faCalendarDays} style={{ marginRight: '10px' }} />
-                            Mantenimientos
-                        </Nav.Link>
-                        <Nav.Link href="#">
-                            <FontAwesomeIcon icon={faCalendarDays} style={{ marginRight: '10px' }} />
-                            Control Mantenimientos
-                        </Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-
-            <div className="row h-100">
-                {/* Sidebar para pantallas grandes */}
-                <div className={`col-auto col-lg-3 border-end p-0 menu d-none d-lg-block`}>
-                    <div className="d-flex flex-column h-100 gap-2">
-                        <div className="profile-header d-flex align-items-center">
-                            <img src={logo} alt="Profile" className="profile-img" />
-                            <div className="title-profile">
-                                <h5 className="profile-title">P&A Aires Acondicionados</h5>
-                                <p className="profile-subtitle">Control de mantenimientos</p>
-                            </div>
-                        </div>
-
-                        <Nav className="flex-column">
-                            <Nav.Link className="nav-item" href="#" onClick={() => setSelectedComponent('Home')}>
-                                <FontAwesomeIcon icon={faHome} style={{ marginRight: '10px' }} />
-                                Home
-                            </Nav.Link>
-                            <Nav.Link className="nav-item" href="#" onClick={() => setSelectedComponent('Usuarios')}>
-                                <FontAwesomeIcon icon={faUsers} style={{ marginRight: '10px' }} />
-                                Usuarios
-                            </Nav.Link>
-                            <Nav.Link className="nav-item" href="#">
-                                <FontAwesomeIcon icon={faCalendarDays} style={{ marginRight: '10px' }} />
-                                Equipos
-                            </Nav.Link>
-                            <Nav.Link className="nav-item" href="#">
-                                <FontAwesomeIcon icon={faCalendarDays} style={{ marginRight: '10px' }} />
-                                Clientes
-                            </Nav.Link>
-                            <Nav.Link className="nav-item" href="#">
-                                <FontAwesomeIcon icon={faCalendarDays} style={{ marginRight: '10px' }} />
-                                Mantenimientos
-                            </Nav.Link>
-                            <Nav.Link className="nav-item" href="#">
-                                <FontAwesomeIcon icon={faCalendarDays} style={{ marginRight: '10px' }} />
-                                Control Mantenimientos
-                            </Nav.Link>
-                        </Nav>
-
-                        <div className="profile-header-user d-flex align-items-center">
-                            <img src={adminLogo} alt="Admin" className="profile-img-user" />
-                            <div className="title-profile">
-                                <h5 className="profile-title-user">{userData.name} {userData.lastName}</h5>
-                                <p className="profile-subtitle-user">{userData.email}</p>
-                            </div>
-                        </div>
-
-                        <Nav className="flex-column">
-                            <Nav.Link className="nav-item" href="#">
-                                <FontAwesomeIcon icon={faGear} style={{ marginRight: '10px' }} />
-                                Configuración
-                            </Nav.Link>
-                            <Nav.Link className="nav-item" href="#" onClick={handleLogoutClick}>
-                                <FontAwesomeIcon icon={faRightFromBracket} style={{ marginRight: '10px' }} />
-                                Cerrar Sesión
-                            </Nav.Link>
-                        </Nav>
-                    </div>
+        {/* Navbar para pantallas pequeñas */}
+        <Navbar bg="light" expand="lg" className="border-bottom d-lg-none">
+          <Navbar.Brand href="#">
+            <img src={logo} alt="Profile" className="profile-img" />
+            P&A Aires Acondicionados
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setMenuOpen(!menuOpen)}>
+            <FontAwesomeIcon icon={faBars} />
+          </Navbar.Toggle>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#" onClick={() => setSelectedComponent('Home')}>
+                <FontAwesomeIcon icon={faHome} style={{ marginRight: '10px' }} />
+                Home
+              </Nav.Link>
+              <Nav.Link href="#" onClick={() => setSelectedComponent('Usuarios')}>
+                <FontAwesomeIcon icon={faUsers} style={{ marginRight: '10px' }} />
+                Usuarios
+              </Nav.Link>
+              <Nav.Link href="#">
+                <FontAwesomeIcon icon={faCalendarDays} style={{ marginRight: '10px' }} />
+                Equipos
+              </Nav.Link>
+              <Nav.Link href="#">
+                <FontAwesomeIcon icon={faCalendarDays} style={{ marginRight: '10px' }} />
+                Clientes
+              </Nav.Link>
+              <Nav.Link href="#">
+                <FontAwesomeIcon icon={faCalendarDays} style={{ marginRight: '10px' }} />
+                Mantenimientos
+              </Nav.Link>
+              <Nav.Link href="#">
+                <FontAwesomeIcon icon={faCalendarDays} style={{ marginRight: '10px' }} />
+                Control Mantenimientos
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+  
+        <div className="row h-100">
+          {/* Sidebar para pantallas grandes */}
+          <div className={`col-auto col-lg-3 border-end p-0 menu d-none d-lg-block`}>
+            <div className="d-flex flex-column h-100 gap-2">
+              <div className="profile-header d-flex align-items-center">
+                <img src={logo} alt="Profile" className="profile-img" />
+                <div className="title-profile">
+                  <h5 className="profile-title">P&A Aires Acondicionados</h5>
+                  <p className="profile-subtitle">Control de mantenimientos</p>
                 </div>
-
-                {/* Contenido principal */}
-                <div className="col-lg-9">
-                    {selectedComponent === 'Usuarios' && <ListUsers />}
+              </div>
+  
+              <Nav className="flex-column">
+                <Nav.Link className="nav-item" href="#" onClick={() => setSelectedComponent('Home')}>
+                  <FontAwesomeIcon icon={faHome} style={{ marginRight: '10px' }} />
+                  Home
+                </Nav.Link>
+                <Nav.Link className="nav-item" href="#" onClick={() => setSelectedComponent('Usuarios')}>
+                  <FontAwesomeIcon icon={faUsers} style={{ marginRight: '10px' }} />
+                  Usuarios
+                </Nav.Link>
+                <Nav.Link className="nav-item" href="#">
+                  <FontAwesomeIcon icon={faCalendarDays} style={{ marginRight: '10px' }} />
+                  Equipos
+                </Nav.Link>
+                <Nav.Link className="nav-item" href="#">
+                  <FontAwesomeIcon icon={faCalendarDays} style={{ marginRight: '10px' }} />
+                  Clientes
+                </Nav.Link>
+                <Nav.Link className="nav-item" href="#">
+                  <FontAwesomeIcon icon={faCalendarDays} style={{ marginRight: '10px' }} />
+                  Mantenimientos
+                </Nav.Link>
+                <Nav.Link className="nav-item" href="#">
+                  <FontAwesomeIcon icon={faCalendarDays} style={{ marginRight: '10px' }} />
+                  Control Mantenimientos
+                </Nav.Link>
+              </Nav>
+  
+              <div className="profile-header-user d-flex align-items-center">
+                <img src={adminLogo} alt="Admin" className="profile-img-user" />
+                <div className="title-profile">
+                  <h5 className="profile-title-user">{userData.name} {userData.lastName}</h5>
+                  <p className="profile-subtitle-user">{userData.email}</p>
                 </div>
-
-                {/* Modal de confirmación de cierre de sesión */}
-                <ConfirmationModal
-                    show={showLogoutModal}
-                    onHide={handleCancelLogout}
-                    onConfirm={handleConfirmLogout}
-                    title="Cierre de Sesión"
-                    bodyText="¿Estás seguro de que deseas cerrar sesión?"
-                    confirmText={loading ? <Spinner animation="border" size="sm" /> : "Sí"}
-                    cancelText="No"
-                />
-
-                {/* Spinner global que se muestra mientras el logout está en proceso */}
-                {loading && (
-                    <div className="loading-overlay">
-                        <Spinner animation="border" role="status">
-                            <span className="visually-hidden">Cargando...</span>
-                        </Spinner>
-                    </div>
-                )}
+              </div>
+  
+              <Nav className="flex-column">
+                <Nav.Link className="nav-item" href="#">
+                  <FontAwesomeIcon icon={faGear} style={{ marginRight: '10px' }} />
+                  Configuración
+                </Nav.Link>
+                <Nav.Link className="nav-item" href="#" onClick={handleLogoutClick}>
+                  <FontAwesomeIcon icon={faRightFromBracket} style={{ marginRight: '10px' }} />
+                  Cerrar Sesión
+                </Nav.Link>
+              </Nav>
             </div>
+          </div>
+  
+          {/* Contenido principal */}
+          <div className="col-lg-9">
+            {selectedComponent === 'Usuarios' && <ListUsers />}
+          </div>
+  
+          {/* Modal de confirmación de cierre de sesión */}
+          <ConfirmationModal
+            show={showLogoutModal}
+            onHide={handleCancelLogout}
+            onConfirm={handleConfirmLogout}
+            title="Cierre de Sesión"
+            bodyText="¿Estás seguro de que deseas cerrar sesión?"
+            confirmText={loading ? <Spinner animation="border" size="sm" /> : "Sí"}
+            cancelText="No"
+          />
+  
+          {/* Spinner global que se muestra mientras el logout está en proceso */}
+          {loading && (
+            <div className="loading-overlay">
+              <Spinner animation="border" role="status">
+                <span className="visually-hidden">Cargando...</span>
+              </Spinner>
+            </div>
+          )}
         </div>
+      </div>
     );
-};
+  };
 
 export default AdminDashboard;
